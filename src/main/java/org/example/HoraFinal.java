64dae56e14902dc45d7e87816d4e73e7b532f39b
Jorge-Cammarota <Jorge-Cammarota>
2023-09-12 13:41:31 -0300

    atualizaçao

---
+++ src/main/java/org/example/HoraFinal.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class HoraFinal {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int horaInicial = sc.nextInt();
+        int horaFinal = sc.nextInt();
+
+        int duracao;
+        if (horaInicial < horaFinal) {
+            duracao = horaFinal - horaInicial;
+        }
+        else {
+            duracao = 24 - horaInicial + horaFinal;
+        }
+
+        System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
+
+        sc.close();
+    }
+}
